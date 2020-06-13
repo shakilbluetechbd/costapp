315e523032625220b30468da9c6f84573a5b305a
Md Shakil Mahmud <shakilssac@gmail.com>
2020-06-14 03:25:08 +0600

    build

---
+++ build/precache-manifest.a1b3c4f99ee0445cde06768a77d9b499.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9398058ccd5c9c89220540b6e1f2c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79f85d1af8664349df19",
+    "url": "/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "c083f0045477b55f2611",
+    "url": "/static/css/main.66c7f68d.chunk.css"
+  },
+  {
+    "revision": "79f85d1af8664349df19",
+    "url": "/static/js/2.12c1a872.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.12c1a872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c083f0045477b55f2611",
+    "url": "/static/js/main.a54c550c.chunk.js"
+  },
+  {
+    "revision": "0559536f84e7c820f3af",
+    "url": "/static/js/runtime-main.d339e538.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
